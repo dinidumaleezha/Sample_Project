0246ac176028ab58176fd23827d6cf8706296f1d
Dinidu Maleesha <118754787+dinidumaleezha@users.noreply.github.com>
2024-09-22 11:52:13 +0530

    v5
    Network_Erorr

---
+++ Sample_Project/src/sample_project/Network_Erorr.java
@@ -0,0 +1,135 @@
+package sample_project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import javax.swing.JOptionPane;
+
+public class Network_Erorr extends javax.swing.JFrame {
+
+    private static final String dbuserName = "root";
+    private static final String password = "";
+    private static final String baseUrl = "jdbc:mysql://localhost:3306/sample_project";
+    
+    private Connection config = null;
+    private PreparedStatement pst = null;
+    private ResultSet result = null;
+    private int number1;
+    
+    public Network_Erorr() {
+        initComponents();
+    }
+    
+    public void db_Config(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            config = DriverManager.getConnection(baseUrl, dbuserName, password);
+            pst = config.prepareStatement("select * from users");
+            result = pst.executeQuery();
+            ResultSetMetaData setData = result.getMetaData();
+            number1 = setData.getColumnCount();
+           
+            Network_Erorr Erorr = new Network_Erorr();
+            Erorr.setVisible(true);
+            
+            Login_Frame login = new Login_Frame();
+            login.setVisible(true);
+
+        } catch (Exception ex){}
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/sample_project/404-error.png"))); // NOI18N
+
+        jButton1.setBackground(new java.awt.Color(0, 128, 255));
+        jButton1.setFont(new java.awt.Font("Segoe UI Semilight", 1, 14)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Try again");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI Semilight", 1, 14)); // NOI18N
+        jLabel2.setText("Database connection failed");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(113, 113, 113))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(96, 96, 96))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 329, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 34, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(20, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        db_Config();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Network_Erorr().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
--- Sample_Project/src/sample_project/Sample_Project.java
+++ Sample_Project/src/sample_project/Sample_Project.java
@@ -4,20 +4,46 @@
  */
 package sample_project;
 
-/**
- *
- * @author malee
- */
-public class Sample_Project {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import javax.swing.JOptionPane;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Sample_Project {
+    
+    private static final String dbuserName = "root";
+    private static final String password = "";
+    private static final String baseUrl = "jdbc:mysql://localhost:3306/sample_project";
+    
+    private Connection config = null;
+    private PreparedStatement pst = null;
+    private ResultSet result = null;
+    private int number1;
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        Login_Frame login = new Login_Frame();
-         login.setVisible(true);
+       
+        Sample_Project db_config = new Sample_Project();
+        db_config.db_Config();
+         
     }
     
+    public void db_Config(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            config = DriverManager.getConnection(baseUrl, dbuserName, password);
+            pst = config.prepareStatement("select * from users");
+            result = pst.executeQuery();
+            ResultSetMetaData setData = result.getMetaData();
+            number1 = setData.getColumnCount();
+            Login_Frame login = new Login_Frame();
+            login.setVisible(true);
+        
+        } catch (Exception ex){
+            Network_Erorr Erorr = new Network_Erorr();
+            Erorr.setVisible(true);
+        }
+        
+    }
 }
